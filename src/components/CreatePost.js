56c381ecdc9a59a860ff2ec93ccaf7872f81566e
gkundan <beingkundan188@gmail.com>
2023-05-04 10:37:53 +0530

    chnage the create post inputs make it larger

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -8,12 +8,26 @@ function CreatePost() {
   const [loading, setLoading] = useState(false);
   const title = useFormInput('');
   const subtitle = useFormInput('');
+  const category = useFormInput('');
   const content = useFormInput('');
+  const author = useFormInput('');
+  const image = useFormInput('');
   const navigate = useNavigate();
 
   async function handleSubmit(e) {
     e.preventDefault();
     setLoading(true);
+
+    // Check if all required fields are filled out
+    if (
+      title.value.trim() === '' ||
+      subtitle.value.trim() === '' ||
+      content.value.trim() === ''
+    ) {
+      alert('Please fill out all required fields');
+      setLoading(false);
+      return;
+    }
 
     try {
       // Save post to Firestore
@@ -21,6 +35,7 @@ function CreatePost() {
         title: title.value,
         subtitle: subtitle.value,
         content: content.value,
+        category: category.value,
         createdAt: new Date(),
       });
       console.log('Post successfully created with ID:', docRef.id);
@@ -45,14 +60,27 @@ function CreatePost() {
           <input type="text" {...title} />
         </div>
         <div className="form-field">
-          <label>Sub Title</label>
+          <label>Subtitle</label>
           <input type="text" {...subtitle} />
         </div>
         <div className="form-field">
-          <label>Content</label>
-          <textarea name="" id="" cols="30" rows="10" {...content}></textarea>
+          <label>Category</label>
+          <input type="text" {...category} />
         </div>
 
+        <div className="form-field">
+          <label>Content</label>
+          <textarea {...content}></textarea>
+        </div>
+
+        <div className="form-field">
+          <label>Author</label>
+          <input type="text" {...author} />
+        </div>
+        <div className="form-field">
+          <label>Image URL</label>
+          <input type="text" {...image} />
+        </div>
         <button className="create-post-btn" disabled={loading}>
           {loading ? 'Creating...' : 'Create Post'}
         </button>
